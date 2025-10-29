7de4a89ab0f19fdf52b28e98cc6dd7f20edc4cbc
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-10-29 15:29:43 +0545

    feat:setup microservcies

---
+++ second/src/main/java/com/example/demo/FiratApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiratApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(FiratApplication.class,args);
+	}
+
+}
